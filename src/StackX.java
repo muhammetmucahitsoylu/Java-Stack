b9e1b922530fc1285a1691477a05ff9b3d2c18be
Muhammet Mücahit Soylu <88398387+muhammetmucahitsoylu@users.noreply.github.com>
2026-02-25 12:21:53 +0100

    Add files via upload

--- src/StackX.java
+++ src/StackX.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class StackX {
+public class StackX {
+
+    private char[] stackArray;
+    private int maxSize;
+    private int top;
+
+    public StackX(int max) {
+        this.maxSize = max;
+        this.stackArray = new char[maxSize];
+        this.top = -1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == maxSize - 1;
+    }
+
+    public void push(char a) {
+        if (!isFull()) {
+            stackArray[++top] = a;
+        }
+    }
+
+    public char pop() {
+        if (!isEmpty()) {
+            return stackArray[top--];
+        }
+        return '\0';
+    }
+
+    public char peek() {
+        if (!isEmpty()) {
+            return stackArray[top];
+        }
+        return '\0';
+    }
+
+    public void clear() {
+        this.top = -1;
+    }
 }
--- src/StackXApp.java
+++ src/StackXApp.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class StackXApp {
+public class StackXApp {
+    public static void main(String[] args) {
+
+
+        StackX stack = new StackX(5);
+
+        stack.push('U');
+        stack.push('C');
+        stack.push('S');
+        stack.push('r');
+        stack.push('p');
+        stack.push('l');
+        stack.push('ğ');
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop() + "");
+        }
+
+        stack.clear();
+        System.out.println("Stack: "+ stack.isEmpty());
+
+        stack.push('U');
+        stack.push('C');
+        stack.push('S');
+
+        System.out.println("Stack peek:"+stack.peek());
+
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop() + "");
+        }
+
+
+
+
+    }
 }
